35d1bf89ea72d3517e3cce5d0d2a24523dcc248a
DiazRock <corolariodiaz@gmail.com>
2024-08-22 00:39:40 +0300

    Added swagger docs

--- main.py
+++ main.py
@@ -5,7 +5,20 @@ from fastapi.staticfiles import StaticFiles
 from controllers.health import router as health_router
 from controllers.screenshots import router as screenshots_router
 
-app = FastAPI()
+app = FastAPI(
+    title="My Screenshot API",
+    description="This API allows you to take screenshots of web pages and store them.",
+    version="1.0.0",
+    contact={
+        "name": "Alejandro Diaz Roque",
+        "url": "https://github.com/DiazRock",
+        "email": "corolariodiaz@gmail.com",
+    },
+    license_info={
+        "name": "MIT License",
+        "url": "https://opensource.org/licenses/MIT",
+    }
+)
 
 # Register the routers
 app.include_router(health_router)
