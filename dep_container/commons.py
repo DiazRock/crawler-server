ccf5a22ed0b2d4915d8bf8c478325d3a219afe2a
DiazRock <corolariodiaz@gmail.com>
2024-08-21 17:53:33 +0300

    New dep_container declarations

--- dep_container/commons.py
+++ dep_container/commons.py
@@ -1,10 +1,27 @@
 import logging
 from functools import lru_cache
+from typing import Generator
+from repositories.screenshot_repository import ScreenshotRepository
 from services.crawler import Crawler
+from pymongo import MongoClient
+from pymongo.database import Database
+from pathlib import Path
+from fastapi import Depends
 
 
-def get_crawler_service():
-    return Crawler()
+BASE_DIR = Path("screenshots")
+BASE_DIR.mkdir(exist_ok=True)
+
+
+@lru_cache(maxsize=None)
+def get_db_session()-> Generator[Database]:
+    client = MongoClient("mongodb://localhost:27017")
+    db = client['screenshots_db']
+    try:
+        yield db
+    finally:
+        client.close()
+
 
 @lru_cache(maxsize=None)
 def get_logger():
@@ -15,3 +32,12 @@ def get_logger():
     return logging.getLogger(__name__)
 
 
+def get_screenshot_repository(db: MongoClient = Depends(get_db_session)):
+    return ScreenshotRepository(db)
+
+
+def get_crawler_service(repository = Depends(get_screenshot_repository)):
+    return Crawler(
+        repository,
+        BASE_DIR
+    )
