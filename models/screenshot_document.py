0880974928cebb5ace1e85b881bcaf4e4f5b5f69
DiazRock <corolariodiaz@gmail.com>
2024-08-25 15:55:47 +0300

    Added validation

--- models/screenshot_document.py
+++ models/screenshot_document.py
@@ -1,11 +1,13 @@
-from pydantic import BaseModel, Field, HttpUrl
-from typing import List
+import uuid
+from pydantic import BaseModel, Field, HttpUrl, AfterValidator
+from typing import List, Annotated
 from datetime import datetime
 from bson import ObjectId
 
+HttpUrlString = Annotated[HttpUrl, AfterValidator(lambda v: str(v))]
 class ScreenshotDocument(BaseModel):
-    id: str = Field(default_factory=str, alias="_id")
-    start_url: HttpUrl
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()), alias="_id")
+    start_url: HttpUrlString
     screenshots: List[str]
     timestamp: datetime
 
